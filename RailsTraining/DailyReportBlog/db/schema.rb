ee408a3c49864ee2ec6290d21558cd8f357c081a
Jun HASHIMOTO <jun.hashimoto.0204@gmail.com>
2015-04-19 17:22:41 +0900

    fix project

--- RailsTraining/DailyReportBlog/db/schema.rb
+++ RailsTraining/DailyReportBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419075722) do
+ActiveRecord::Schema.define(version: 20150419082120) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
