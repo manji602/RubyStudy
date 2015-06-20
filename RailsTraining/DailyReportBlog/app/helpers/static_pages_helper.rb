cb323aa82c51056e702c5b6b0015d7f1f9e23e02
Jun Hashimoto <jun.hashimoto.0204@gmail.com>
2015-06-20 13:45:01 +0900

    fix home

--- RailsTraining/DailyReportBlog/app/helpers/static_pages_helper.rb
+++ RailsTraining/DailyReportBlog/app/helpers/static_pages_helper.rb
@@ -1,2 +1,7 @@
 module StaticPagesHelper
+  def has_blog?
+    return false unless signed_in?
+    return false unless current_user.blog.nil?
+    return true
+  end
 end
--- RailsTraining/DailyReportBlog/config/routes.rb
+++ RailsTraining/DailyReportBlog/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   match 'signout', to: 'sessions#destroy', via: 'delete'
 
   resources :users
-  resources :comments
+  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
