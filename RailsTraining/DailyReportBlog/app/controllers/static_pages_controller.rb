4a17dc99396b181fdedebeccb13d1423421e175f
Jun Hashimoto <jun.hashimoto.0204@gmail.com>
2015-06-21 10:10:43 +0900

    create entry form

--- RailsTraining/DailyReportBlog/app/controllers/static_pages_controller.rb
+++ RailsTraining/DailyReportBlog/app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
+  include StaticPagesHelper
   before_action :signed_in_user, only: [:settings]
+
   def home
+    if has_blog?
+      @blog = current_blog
+    end
   end
 
   def settings
